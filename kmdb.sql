a5a54ec45a5b680632650e9cbcbafae2ce205615
Andrew Wallen <andrew.wallen@kellogg.northwestern.edu>
2025-01-24 03:55:31 +0000

    Inserted data into the studio and movies tables

--- kmdb.sql
+++ kmdb.sql
@@ -153,6 +153,33 @@ CREATE TABLE top_cast(
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 -- TODO!
+INSERT INTO studio(
+    studio_name
+)
+VALUES (
+    "Warner Bros."
+);
+
+INSERT INTO movies(
+    movie_name,
+    release_year,
+    mpaa_rating,
+    studio_id
+)
+VALUES (
+    "Batman Begins",
+    2005, 
+    "PG-13",
+    1
+),
+    ("The Dark Knight",
+    2008,
+    "PG-13",
+    1),
+    ("The Dark Knight Rises",
+    2012,
+    "PG-13",
+    1);
 
 -- Prints a header for the movies output
 .print "Movies"
